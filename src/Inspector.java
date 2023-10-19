791663e7fd10dbe0ebcfcd4efa79c76f6e9a4946
youssefsamaan2003 <127629577+youssefsamaan2003@users.noreply.github.com>
2023-10-19 09:19:32 -0600

    completed the first two requirements, the name of the declaring class and the name of the immediate superclass

--- src/Inspector.java
+++ src/Inspector.java
@@ -5,6 +5,13 @@ public class Inspector {
 
     public void inspect(Object obj, boolean recursive) {
         Class<?> clazz = obj.getClass();
+        // 1. The name of the declaring class
+        System.out.println("Declaring Class: " + clazz.getName());
 
+        // 2. The name of the immediate superclass
+        Class<?> superClass = clazz.getSuperclass();
+        if (superClass != null) {
+            System.out.println("Immediate Superclass: " + superClass.getName());
+        }
     }
 }
