6cdb4bfbcad6341bbc2cdad5057f2fca198a48f1
youssefsamaan2003 <127629577+youssefsamaan2003@users.noreply.github.com>
2023-10-19 09:14:42 -0600

    Initial Commit

---
+++ src/Inspector.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.*;
+import java.util.Arrays;
+
+public class Inspector {
+
+    public void inspect(Object obj, boolean recursive) {
+        Class<?> clazz = obj.getClass();
+
+    }
+}
